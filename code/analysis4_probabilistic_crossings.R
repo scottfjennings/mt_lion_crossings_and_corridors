cd4a7f0d89badab435e0ad529f01e68c31ae9bf4
scottfjennings <scott.f.jennings@gmail.com>
2024-06-10 14:56:49 -0700

    plotting function moved to utilities; other cleanup and annotation

--- code/analysis4_probabilistic_crossings.R
+++ code/analysis4_probabilistic_crossings.R
@@ -62,9 +62,11 @@ zz %>%
   geom_point(aes(x = prob.size, y = value, color = name)) +
   stat_smooth(aes(x = prob.size, y = value, color = name))
 
-# helper functions ----
+# 
 
-#' bbmm_to_UD
+# now the data processing steps ----
+# 1. make BBMM Utilization Distribution ----
+#' bbmm_to_UD 
 #'
 #' create a Utilization Distribution from a Brownian Bridge Movement Model object
 #'
@@ -94,9 +96,9 @@ system.time(
 )
 # ~ 20 sec
 names(all_bbmm_ud) <- names(all_clusters_bbmm)
+saveRDS(all_bbmm_ud, here("model_objects/all_bbmm_ud"))
 
-
-
+# 2. create a raster for each UD ---- 
 #' UD_to_raster
 #' 
 #' create a raster of a Utilization Distribution from the BBMM model objects created in analysis3_fit_BBMM. should work on any output from BBMM::brownian.bridge()
@@ -123,10 +125,10 @@ system.time(
   all_ud_rast <- map(all_bbmm_ud, UD_to_raster), gcFirst = TRUE
 )
 # ~ 27 sec
-
 names(all_ud_rast) <- names(all_clusters_bbmm)
+saveRDS(all_ud_rast, here("model_objects/all_ud_rast"))
 
-
+# 3. create a box around each crossing step ----
 #' get_step_box
 #' 
 #' create a box centered at the crossing step, with the side parallel to the step as long as the step and the other side = 3x the step length. This is used to segment the BBMM UD to just the area around the crossing step
@@ -182,7 +184,7 @@ system.time(
 names(all_step_boxes) <- crossing_steps
 saveRDS(all_step_boxes, here("model_objects/all_step_boxes"))
 
-
+# 4. trim the BBMM UD to the step box ----
 #' trim_ud_to_step
 #'
 #' overlay the rectangle on the 90% UD to get just the UD that is between the 2 step end points
@@ -206,8 +208,9 @@ system.time(
 )
 
 names(all_ud_trim_to_step) <- crossing_steps
+saveRDS(all_ud_trim_to_step, here("model_objects/all_ud_trim_to_step"))
 
-# now id the section of road that is within the 90% UD ----
+# 5. id the section of road that is within the trimmed 90% UD ----
 
 # zcrossing.step = crossing_steps$crossing.step[272]
 
@@ -254,54 +257,20 @@ system.time(
   all_bbmm_road_slices <- map(crossing_steps, get_bbmm_crossing), gcFirst = TRUE
 ) # ~420 sec, or 179
 names(all_bbmm_road_slices) <- crossing_steps
-
 saveRDS(all_bbmm_road_slices, here("model_objects/all_bbmm_road_slices"))
 
-
-road_crossing_plotter <- function(zcrossing.step) {
-  
-  # read UD raster  
-  rp <- all_ud_rast[[zcrossing.step]]
-  
-  # clip the road layer to cut down computing time for the main st_intersection below
-  road_slicer <- st_bbox(rp)  %>% st_as_sfc()
-  road_slice <- st_intersection(napa_sonoma_rds_utm, road_slicer)
-  
-ggplot2::ggplot() +
-  geom_sf(data = all_ud_rast[[zcrossing.step]]) +
-  #tidyterra::geom_spatraster(data = all_ud_rast[[zcrossing.step]]) +
-  #geom_sf(data = all_step_boxes[[zcrossing.step]], fill = NA) +
-  geom_sf(data = all_ud_trim_to_step[[zcrossing.step]], color = "red", fill = NA, linewidth = 2)  +
-  geom_sf(data = road_slice, color = "gray") +
-  geom_sf(data = all_bbmm_road_slices[[zcrossing.step]], color = "blue", linewidth = 3)  +
-  geom_path(data = filter(crossing_clusters_gps, crossing.step == zcrossing.step), aes(x = easting, y = northing)) +
-  geom_point(data = filter(crossing_clusters_gps, crossing.step == zcrossing.step), aes(x = easting, y = northing)) +
-  geom_point(data = filter(crossing_clusters_gps, crossing.step == zcrossing.step, (step.id == zcrossing.step | lag(step.id) == zcrossing.step)), aes(x = easting, y = northing, color = step.id), size = 4) +
-  labs(title = zcrossing.step,
-       x = "",
-       y = "",
-       color = "",
-       fill = "90% UD") +
-  coord_sf(datum = st_crs(26910))
-}
-
+# check how these crossings look ----
 # example tricky crossings
-# road_crossing_plotter("P1_37472_11128")
-# road_crossing_plotter("P1_37472_11324")
-# road_crossing_plotter("P13_37473_35018")
-# road_crossing_plotter("P13_37473_35901")
-# road_crossing_plotter("P16_37473_50936") # wide UD
+# prob_road_crossing_plotter("P1_37472_11128")
+# prob_road_crossing_plotter("P1_37472_11324")
+# prob_road_crossing_plotter("P13_37473_35018")
+# prob_road_crossing_plotter("P13_37473_35901")
+# prob_road_crossing_plotter("P16_37473_50936") # wide UD
 
 # funky movements
-# road_crossing_plotter("P16_37473_50642")
+# prob_road_crossing_plotter("P16_37473_50642")
 
-road_crossing_plotter(crossing_steps[300])
-
-
-# next the crossed road segments need some tidying ----
-
-prob_crossings <- readRDS(here("model_objects/prob_crossings"))
-
+# 6. combine adjacent road segments into continuous objects ---- 
 #' combine_continuous
 #' combines continuous segments of road into single object
 #'
@@ -348,10 +317,12 @@ system.time(
 )
 # 171 sec
 names(all_bbmm_road_slices_continuous) <- names(all_bbmm_road_slices)
+saveRDS(all_bbmm_road_slices_continuous, here("model_objects/all_bbmm_road_slices_continuous"))
 
 
 plot(all_bbmm_road_slices_continuous[[539]])
 
+# 7. check how many continuous road objects each step has ----
 #' num_segment_checker
 #' 
 #' check how many segments remain in the clipped road layer after touching and close segments were combined with combine_continuous()
@@ -374,6 +345,7 @@ filter(num_segments, num.segments != 1) %>%
 
 zcrossing.step = "P1_32189_6078"
 
+# 8. filter out any continuous road objects that aren't crossed by the straight line step ----
 #' confirm_bbmm_rd_cross
 #' 
 #' confirm that the combined segments from combine_continuous() are along the straight line between the 2 crossing step end points
@@ -419,13 +391,13 @@ system.time(
 # 217 sec
 
 names(bbmm_crossing_steps) <- crossing_steps
+saveRDS(bbmm_crossing_steps, here("model_objects/bbmm_crossing_steps"))
 
 
+# check again how crossings look ----
 zstep = "P1_37472_12612"
 
-road_crossing_plotter(zstep) +
-  geom_sf(data = bbmm_crossing_steps[[zstep]] %>% st_as_sf(), color = "red", linewidth = 2) +
-  coord_sf(datum = st_crs(26910))
+prob_road_crossing_plotter(zstep) 
 
 # works well
 # zstep = "P16_37473_51563"
@@ -438,145 +410,3 @@ road_crossing_plotter(zstep) +
 
 
 
-
-
-# original big clunky get_bbmm_crossing ----------------------------------------
-  #' get_bbmm_crossing
-  #' 
-  #' id the section of road that is within the 90% UD 
-  #'
-  #' @param zcrossing.step character string indicating the ID for the cluster of points to evaluate
-  #' @param level UD level in proportion (0-1) format
-  #' @param plot logical, return a plot for checking or return an sf object with the id'ed road segments
-  #'
-  #' @return
-  #' @details
-  #' full UD fitted to 6 steps ends up covering too much road, so get_bbmm_crossing() segments the UD to a block defined by just the crossing step and selects the roads just in that segment.
-  #' 
-  #'
-  #' @examples
-  get_bbmm_crossing <- function(zcrossing.step, level = .90, plot = FALSE) {
-    
-    crossing_cluster <- filter(crossing_clusters_gps, crossing.step == zcrossing.step) %>% 
-      left_join(readRDS(here("data/puma_steps")) %>% 
-                  dplyr::select(step.id, step.dist, step.dur)) %>% 
-      mutate(step.dist.2hr = (step.dist/as.numeric(step.dur)) * 7200,
-             mean.step.length = mean(step.dist.2hr))
-    
-    # get the BBMM model object for the zcrossing.step crossing cluster
-    bbmm <- all_clusters_bbmm[[zcrossing.step]]
-    # convert to data frame, sort on probability, and calculate cumulative probability to allow filtering to level UD level
-    bbmm_df <- do.call(cbind.data.frame, bbmm) %>% 
-      mutate(zindex = row_number()) %>% 
-      arrange(-probability) %>% 
-      mutate(prob.sum = cumsum(probability),
-             crossing.step = zcrossing.step,
-             level.lab = paste(level * 100, "% UD", sep = "")) %>% 
-      arrange(zindex) %>% 
-      filter(prob.sum <= level)
-    
-    # make it raster, UTM  
-    r <- terra::rast(cbind(bbmm_df$x, bbmm_df$y, ceiling(bbmm_df$probability)), type = "xyz")
-    #r <- terra::rast(cbind(bbmm_df$x, bbmm_df$y, bbmm_df$probability), type = "xyz")
-    crs(r) <- "EPSG:26910"
-    
-    # raster to polygon b/c for some reason can't to st_as_sf on a SpatRaster but can on a SpatVector
-    rp <- terra::as.polygons(r, crs = 26910, values = TRUE, digits = 0, aggregate = TRUE)
-    
-    # clip the road layer to cut down computing time for the main st_intersetcion below
-    road_slicer <- st_bbox(r)  %>% st_as_sfc()
-    road_slice <- st_intersection(napa_sonoma_rds_utm, road_slicer)
-    
-    # and finally get the overlap between the UD and road layer to get the estimate of where the crossing actually happened --
-    
-    # -- mask road based on either the distance between the 2 crossing step points or the perpendicular width of the 90% UD --
-    # from https://stackoverflow.com/questions/74844804/finding-a-set-of-equally-spaced-perpendicular-lines-along-boundaries-in-r
-    # this is the first point for the crossing step
-    p1 <- crossing_cluster[3,] %>% st_as_sf(coords = c("easting", "northing"), crs = 26910)
-    # and this is the second point for the crossing step
-    p2 <- crossing_cluster[4,] %>% st_as_sf(coords = c("easting", "northing"), crs = 26910)
-    # the angle of the line between p1 and p2
-    alpha <- (atan((sf::st_coordinates(p1)[2] - sf::st_coordinates(p2)[2]) / (sf::st_coordinates(p1)[1] - sf::st_coordinates(p2)[1])))+pi/2 
-    # the length of the largest easting or northing dimension of the bbmm probability layer - just needed something sufficiently bigger than 1/2 the width of the 90% UD
-    biggest_dim <- max(c(st_bbox(rp)$xmax - st_bbox(rp)$xmin, st_bbox(rp)$ymax - st_bbox(rp)$ymin))
-    
-    # this is the rise and run of the perpendicular line end points
-    x1 <- (biggest_dim/2) * cos(alpha) 
-    y1 <- (biggest_dim/2) * sin(alpha) 
-    
-    # this point defines a perpendicular lines crossing p1 and p2 with length = biggest_dim
-    # need to define 2 points for each line, on opposite sides of the step end point
-    # this is for p1
-    p3 <- bind_rows(data.frame(northing = c(sf::st_coordinates(p1)[2] + y1, sf::st_coordinates(p1)[2] - y1),
-                               easting = c(sf::st_coordinates(p1)[1] + x1, sf::st_coordinates(p1)[1] - x1)))
-    # this is for p2
-    p4 <- bind_rows(data.frame(northing = c(sf::st_coordinates(p2)[2] - y1, sf::st_coordinates(p2)[2] + y1),
-                               easting = c(sf::st_coordinates(p2)[1] - x1, sf::st_coordinates(p2)[1] + x1)))
-    # need to join those 2 lines into to a rectangle, requires the first point be duplicated at the end to close the rectangle
-    p_box <- bind_rows(p3, p4, p3[1,]) 
-    # rectangle into a spatial polygon
-    p_poly <- st_polygon(list(matrix(c(p_box$easting, p_box$northing), ncol = 2))) %>% 
-      st_sfc(crs=26910)
-    # overlay the rectangle on the 90% UD to get just the UD that is between the 2 step end points
-    ud_slice = st_intersection(st_as_sf(rp), p_poly) 
-    
-    
-    bbmm_road_slice <- napa_sonoma_rds_utm %>% 
-      st_as_sf() %>% 
-      st_intersection(st_as_sf(ud_slice)) 
-    
-    # ----  
-    if(plot) {
-      ggplot2::ggplot() +
-        tidyterra::geom_spatraster(data = r) +
-        geom_sf(data = p_poly, fill = NA) +
-        geom_sf(data = ud_slice, color = "red", fill = NA, linewidth = 2)  +
-        geom_sf(data = road_slice, color = "gray") +
-        geom_sf(data = bbmm_road_slice, color = "green", linewidth = 2)  +
-        geom_path(data = crossing_cluster, aes(x = easting, y = northing)) +
-        geom_point(data = crossing_cluster, aes(x = easting, y = northing)) +
-        geom_point(data = crossing_cluster[3:4,], aes(x = easting, y = northing, color = step.id), size = 4) +
-        labs(title = zcrossing.step,
-             x = "",
-             y = "",
-             color = "",
-             fill = "90% UD") +
-        coord_sf(datum = st_crs(26910))
-      
-    } else {
-      return(bbmm_road_slice)
-    }
-  }
-
-
-
-get_bbmm_crossing(zstep, level = .9, plot = TRUE) # good
-
-
-get_bbmm_crossing(crossing_steps[385], level = .9, plot = TRUE) 
-
-# P1_37472_15804
-# P1_37472_11250 has multiple segments
-
-xx <- get_bbmm_crossing(crossing_steps$crossing.step[272], level = .9, plot = FALSE) 
-
-
-system.time(
-  prob_crossings <- map(crossing_steps$crossing.step, safely(get_bbmm_crossing)), gcFirst = TRUE
-)
-
-names(prob_crossings) <- crossing_steps$crossing.step
-
-map(prob_crossings, "error") %>% 
-  compact()
-
-
-prob_crossings <- map(prob_crossings, "result") %>% 
-  compact() 
-
-saveRDS(prob_crossings, here("model_objects/prob_crossings"))
-
-# convert to df if needed, not currently needed
-# prob_crossings_df <- prob_crossings %>% bind_rows(.id = "crossing.step")
-
-          
