a6de39e319d8d2e3b36d9c2a0baac0ba0e256f0f
scottfjennings <scott.f.jennings@gmail.com>
2024-10-09 08:01:44 -0700

    add more helpers

--- code/helper_data.R
+++ code/helper_data.R
@@ -1,16 +1,25 @@
 
 
-keep_road_classes = c("Collector", 
-                      "Freeway", 
-                      "Highway", 
-                      "Arterial"#,
+keep_road_classes = c("Collector" 
+                      , "Freeway" 
+                      , "Highway" 
+                      , "Arterial"
+                      , "Local"
+                      , "Access Road"
+                      , "Other"
                       #"Ramp/Interchange"
 )
 
 
 exclude_labels <- c("Hwy 101 N", "Hwy 12 E")
 
-
+exclude_pumas <- c("P10"  # only collared 2 weeks
+                   , "P15" # only collared 1 week
+                   , "P17" # very little data so likely will break logistic regression models
+                   , "P20" # only collared 1 week
+                   , "P27" # daughter of P16, both died at the same time while P27 still dependent
+                   , "P32" # moved to Hopland, Q thinks data after move are good, but that's outside our study area
+)
 
 
 hr_exclude_pumas <- c("P36", #funky locations, in the ocean
@@ -18,3 +27,6 @@ hr_exclude_pumas <- c("P36", #funky locations, in the ocean
                       "P37", # not totally range resident and 100% HR covers much of sonoma 
                       "P19" # many points outside Sonoma/Napa
 )
+
+
+p31_exclude_segments <- c("Hwy 1_16", "Hwy 1_17", "Hwy 1_18", "Hwy 1_19", "Hwy 1_20", "Hwy 1_21", "Hwy 1_22", "Hwy 1_23")
