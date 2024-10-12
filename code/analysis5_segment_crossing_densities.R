55ddb32579e2e0faaeffc602652da3b1f1c792f4
scottfjennings <scott.f.jennings@gmail.com>
2024-10-12 06:18:00 -0700

    adapt for Arc merge workflow

--- code/analysis5_segment_crossing_densities.R
+++ code/analysis5_segment_crossing_densities.R
@@ -12,24 +12,29 @@ all_bbmm_road_slices <- readRDS(here("model_objects/crossed_bbmm_roads_1step")) 
   bind_rows(., .id = "crossing.step") 
 
 
+bbmm_crossed_equal_seg <- readRDS(here("data/bbmm_crossed_equal_seg")) %>% 
+  bind_rows(., .id = "crossing.step") %>% 
+  select(-X, -seg.length) %>% 
+  mutate(crossed.seg.length = st_length(geometry))
+
+
 napa_sonoma_rds_equal_segs <- readRDS(here("data/napa_sonoma_rds_equal_segs")) %>% 
   bind_rows() %>% 
   rename("road.seg.length" = seg.length)
 
-
+crossing_years <- readRDS(here("data/crossing_clusters_gps_1step")) %>% 
+  mutate(year = year(datetime.local)) %>% 
+  distinct(crossing.step, year)
 
 # create a weighted crossing value based on the proportion of the equal length segment that was included in the BBMM UD
-wt_road_crossed_segs <- all_bbmm_road_slices %>% 
-  select(-X) %>% 
+wt_road_crossed_segs <- bbmm_crossed_equal_seg %>% 
   left_join(napa_sonoma_rds_equal_segs %>% 
                                  data.frame() %>% 
                                  select(-geometry)) %>% 
   mutate(raw.crossing = 1,
          weighted.crossing = as.numeric(crossed.seg.length/road.seg.length)) %>% 
-  full_join(readRDS(here("data/crossing_clusters_gps_1step")) %>% 
-              mutate(year = year(datetime.local)) %>% 
-              distinct(crossing.step, year)) %>% 
-  arrange(label, seg.label)  %>% 
+  full_join(crossing_years) %>% 
+  arrange(label.city, seg.label)  %>% 
   mutate(animal.id = str_extract(crossing.step, "^[^_]+(?=_)"))
   
 saveRDS(wt_road_crossed_segs, here("data/wt_road_crossed_segs"))
