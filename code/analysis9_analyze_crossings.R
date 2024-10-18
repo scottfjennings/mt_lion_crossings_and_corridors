1ae1929f73188bddab927f88bb3e04e614d2470b
scottfjennings <scott.f.jennings@gmail.com>
2024-10-18 08:26:13 -0700

    updating for newbest scales for habitat varbs

--- code/analysis9_analyze_crossings.R
+++ code/analysis9_analyze_crossings.R
@@ -5,8 +5,8 @@ library(tidyverse)
 library(here)
 library(lme4)
 library(AICcmodavg)
-library(merTools)
-library(sf)
+#library(merTools)
+#library(sf)
 
 
 
@@ -14,24 +14,20 @@ source(here("code/helper_data.R"))
 
 
 # final data prep ----
-puma_ok_bridges_per_seg <- readRDS(here("data/puma_ok_bridges_per_seg")) %>% 
-  data.frame() %>% 
-  dplyr::select(-geometry)
-
+# not including varbs for traffic conditions for TWS/CADFW mt lion working group
 summed_crossing_analysis_table <- readRDS(here("data/habitat_varbs_scales")) %>% 
-  left_join(readRDS(here("data/puma_ok_bridges_per_seg")) %>% 
-              data.frame() %>% 
-              dplyr::select(-geometry)) %>% 
+  left_join(readRDS(here("data/puma_ok_bridges_per_seg")) ) %>% 
   mutate(num.bridge = replace_na(num.bridge, 0)) %>% 
-  left_join(readRDS(here("data/equal_length_classes")) %>% 
-              dplyr::select(-road.seg.length)) %>% 
+#  left_join(readRDS(here("data/equal_length_classes")) %>% 
+#             dplyr::select(-road.seg.length)) %>% 
   filter(!animal.id %in% hr_exclude_pumas,
-         !seg.label %in% p31_exclude_segments,
-         !is.na(classes))
+         !seg.label %in% p31_exclude_segments#,
+         #!is.na(classes)
+         )
 
 
-summed_crossing_analysis_table_long <- summed_crossing_analysis_table %>% 
-  pivot_longer(cols = c(tot.raw.cross, tot.wt.cross), names_to = "cross.type", values_to = "cross.value")
+# summed_crossing_analysis_table_long <- summed_crossing_analysis_table %>% 
+#  pivot_longer(cols = c(tot.raw.cross, tot.wt.cross), names_to = "cross.type", values_to = "cross.value")
 
 
 
@@ -53,9 +49,20 @@ summed_crossing_analysis_table_long %>%
   stat_smooth(aes(y = cross.value, x = dev.60, color = as.factor(year)), method = "lm") +
   facet_wrap(~animal.id, scales = "free_y")
 
+# fit models ----
 
+
+# compare lmer vs glmer
+
+dev300.bridge.lmer = lmer(tot.raw.cross ~ dev.300 * num.bridge + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE)
+dev300.bridge.glmer = glmer(tot.raw.cross ~ dev.300 * num.bridge + (1|animal.id), data = summed_crossing_analysis_table, family = "poisson")
+aictab(list(dev300.bridge.glmer, dev300.bridge.lmer))
+
+# dev.300 is best for tot.raw.cross
+# dev.60 is best for tot.wt.cross
+# currently using tot.raw.cross so will need to change model structures and names in the functions below if using tot.wt.cross instead
 # modeling crossings with animal.id and year as random effects ----
-fit_summed_crossing_mods <- function(zcross) {
+fit_summed_crossing_mods_pumayr <- function(zcross) {
   
   df <- summed_crossing_analysis_table_long %>% 
     filter(cross.type == zcross)
@@ -63,26 +70,16 @@ fit_summed_crossing_mods <- function(zcross) {
   zmods <- list(
     cov30 = lmer(cross.value ~ tre.shr.30 + (1|year/animal.id), data = df, REML = FALSE),
     cov300 = lmer(cross.value ~ tre.shr.300 + (1|year/animal.id), data = df, REML = FALSE),
-    dev60 = lmer(cross.value ~ dev.60 + (1|year/animal.id), data = df, REML = FALSE),
+    dev300 = lmer(cross.value ~ dev.300 + (1|year/animal.id), data = df, REML = FALSE),
     bridge = lmer(cross.value ~ num.bridge + (1|year/animal.id), data = df, REML = FALSE),
-    class = lmer(cross.value ~ max.class + (1|year/animal.id), data = df, REML = FALSE),
     # cover and number of bridges
     cov30_bridge = lmer(cross.value ~ tre.shr.30 + num.bridge + (1|year/animal.id), data = df, REML = FALSE),
     cov300_bridge = lmer(cross.value ~ tre.shr.300 + num.bridge + (1|year/animal.id), data = df, REML = FALSE),
-    dev60_bridge = lmer(cross.value ~ dev.60 + num.bridge + (1|year/animal.id), data = df, REML = FALSE),
+    dev300_bridge = lmer(cross.value ~ dev.300 + num.bridge + (1|year/animal.id), data = df, REML = FALSE),
     
     cov30.bridge = lmer(cross.value ~ tre.shr.30 * num.bridge + (1|year/animal.id), data = df, REML = FALSE),
     cov300.bridge = lmer(cross.value ~ tre.shr.300 * num.bridge + (1|year/animal.id), data = df, REML = FALSE),
-    dev60.bridge = lmer(cross.value ~ dev.60 * num.bridge + (1|year/animal.id), data = df, REML = FALSE),
-    
-    # cover and road class
-    cov30_class = lmer(cross.value ~ tre.shr.30 + max.class + (1|year/animal.id), data = df, REML = FALSE),
-    cov300_class = lmer(cross.value ~ tre.shr.300 + max.class + (1|year/animal.id), data = df, REML = FALSE),
-    dev60_class = lmer(cross.value ~ dev.60 + max.class + (1|year/animal.id), data = df, REML = FALSE),
-    
-    cov30.class = lmer(cross.value ~ tre.shr.30 * max.class + (1|year/animal.id), data = df, REML = FALSE),
-    cov300.class = lmer(cross.value ~ tre.shr.300 * max.class + (1|year/animal.id), data = df, REML = FALSE),
-    dev60.class = lmer(cross.value ~ dev.60 * max.class + (1|year/animal.id), data = df, REML = FALSE),
+    dev300.bridge = lmer(cross.value ~ dev.300 * num.bridge + (1|year/animal.id), data = df, REML = FALSE),
     
     intercept = lmer(cross.value ~ 1  + (1|year/animal.id), data = df, REML = FALSE)
     
@@ -96,42 +93,68 @@ fit_summed_crossing_mods <- function(zcross) {
 
 
 
+system.time(
+raw_crossings_mods_pumayr <- fit_summed_crossing_mods_pumayr("tot.raw.cross")
+)
 
-raw_crossings_mods <- fit_summed_crossing_mods("tot.raw.cross")
+# getting singular warning for most/all models
+# and year resid variance is small, so removing year random effect
 
 #
-# modeling crossings with just animal.id as random effect ----
+# modeling raw crossings with just animal.id as random effect ----
 
-fit_summed_crossing_mods <- function(zcross) {
+fit_raw_crossing_mods_puma <- function(zcross) {
   
-  df <- summed_crossing_analysis_table_long %>% 
-    filter(cross.type == zcross)
-  #"animal.id" "year" "tre.shr.30"  "dev.60"     "tre.shr.300" "num.bridge"  "classes"     "max.class  
   zmods <- list(
-    cov30 = lmer(cross.value ~ tre.shr.30 + (1|animal.id), data = df, REML = FALSE),
-    cov300 = lmer(cross.value ~ tre.shr.300 + (1|animal.id), data = df, REML = FALSE),
-    dev60 = lmer(cross.value ~ dev.60 + (1|animal.id), data = df, REML = FALSE),
-    bridge = lmer(cross.value ~ num.bridge + (1|animal.id), data = df, REML = FALSE),
-    class = lmer(cross.value ~ max.class + (1|animal.id), data = df, REML = FALSE),
+    cov30 = lmer(tot.raw.cross ~ tre.shr.30 + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
+    cov300 = lmer(tot.raw.cross ~ tre.shr.300 + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
+    dev300 = lmer(tot.raw.cross ~ dev.300 + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
+    bridge = lmer(tot.raw.cross ~ num.bridge + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
     # cover and number of bridges
-    cov30_bridge = lmer(cross.value ~ tre.shr.30 + num.bridge + (1|animal.id), data = df, REML = FALSE),
-    cov300_bridge = lmer(cross.value ~ tre.shr.300 + num.bridge + (1|animal.id), data = df, REML = FALSE),
-    dev60_bridge = lmer(cross.value ~ dev.60 + num.bridge + (1|animal.id), data = df, REML = FALSE),
+    cov30_bridge = lmer(tot.raw.cross ~ tre.shr.30 + num.bridge + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
+    cov300_bridge = lmer(tot.raw.cross ~ tre.shr.300 + num.bridge + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
+    dev300_bridge = lmer(tot.raw.cross ~ dev.300 + num.bridge + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
     
-    cov30.bridge = lmer(cross.value ~ tre.shr.30 * num.bridge + (1|animal.id), data = df, REML = FALSE),
-    cov300.bridge = lmer(cross.value ~ tre.shr.300 * num.bridge + (1|animal.id), data = df, REML = FALSE),
-    dev60.bridge = lmer(cross.value ~ dev.60 * num.bridge + (1|animal.id), data = df, REML = FALSE),
+    cov30.bridge = lmer(tot.raw.cross ~ tre.shr.30 * num.bridge + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
+    cov300.bridge = lmer(tot.raw.cross ~ tre.shr.300 * num.bridge + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
+    dev300.bridge = lmer(tot.raw.cross ~ dev.300 * num.bridge + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
     
-    # cover and road class
-    cov30_class = lmer(cross.value ~ tre.shr.30 + max.class + (1|animal.id), data = df, REML = FALSE),
-    cov300_class = lmer(cross.value ~ tre.shr.300 + max.class + (1|animal.id), data = df, REML = FALSE),
-    dev60_class = lmer(cross.value ~ dev.60 + max.class + (1|animal.id), data = df, REML = FALSE),
+    intercept = lmer(tot.raw.cross ~ 1  + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE)
     
-    cov30.class = lmer(cross.value ~ tre.shr.30 * max.class + (1|animal.id), data = df, REML = FALSE),
-    cov300.class = lmer(cross.value ~ tre.shr.300 * max.class + (1|animal.id), data = df, REML = FALSE),
-    dev60.class = lmer(cross.value ~ dev.60 * max.class + (1|animal.id), data = df, REML = FALSE),
+  )
+  
+  zmods$aic = aictab(zmods, names(zmods))
+  
+  return(zmods)
+  
+}
+# fit for raw crossings as response
+
+system.time(
+  raw_crossings_mods_puma <- fit_raw_crossing_mods_puma("tot.raw.cross")
+)
+
+
+
+# modeling weighted crossings with just animal.id as random effect ----
+
+fit_wt_crossing_mods_puma <- function() {
+#  60m is thge best scale for % development for weighted crossings
+  zmods <- list(
+    cov30 = lmer(tot.wt.cross ~ tre.shr.30 + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
+    cov300 = lmer(tot.wt.cross ~ tre.shr.300 + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
+    dev60 = lmer(tot.wt.cross ~ dev.60 + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
+    bridge = lmer(tot.wt.cross ~ num.bridge + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
+    # cover and number of bridges
+    cov30_bridge = lmer(tot.wt.cross ~ tre.shr.30 + num.bridge + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
+    cov300_bridge = lmer(tot.wt.cross ~ tre.shr.300 + num.bridge + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
+    dev60_bridge = lmer(tot.wt.cross ~ dev.60 + num.bridge + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
     
-    intercept = lmer(cross.value ~ 1  + (1|animal.id), data = df, REML = FALSE)
+    cov30.bridge = lmer(tot.wt.cross ~ tre.shr.30 * num.bridge + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
+    cov300.bridge = lmer(tot.wt.cross ~ tre.shr.300 * num.bridge + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
+    dev60.bridge = lmer(tot.wt.cross ~ dev.60 * num.bridge + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE),
+    
+    intercept = lmer(tot.wt.cross ~ 1  + (1|animal.id), data = summed_crossing_analysis_table, REML = FALSE)
     
   )
   
@@ -142,26 +165,30 @@ fit_summed_crossing_mods <- function(zcross) {
 }
 
 
-# fit for raw crossings as response
+# fit for weighted crossings as response
 
-raw_crossings_mods <- fit_summed_crossing_mods("tot.raw.cross")
+system.time(
+  wt_crossings_mods_puma <- fit_wt_crossing_mods_puma()
+)
 
 
-znewdat = expand.grid(dev.60 = seq(0, 1, by = 0.1),
-                      num.bridge = seq(0, max(summed_crossing_analysis_table_long$num.bridge))
+
+znewdat = expand.grid(dev.300 = seq(0, 1, by = 0.1),
+                      num.bridge = seq(0, max(summed_crossing_analysis_table$num.bridge))
                       #, animal.id = distinct(summed_crossing_analysis_table_long, animal.id)$animal.id
-                      ) 
+                      ) %>% 
+  mutate(dev.60 = dev.300)
 
 
 
-zpred_raw = predict(raw_crossings_mods$dev60.bridge, znewdat, se.fit = TRUE, type = "response", re.form = NA) %>% 
+zpred_wt = predict(wt_crossings_mods_puma$dev60.bridge, znewdat, se.fit = TRUE, type = "response", re.form = NA) %>% 
   data.frame() %>% 
   bind_cols(znewdat) %>% 
   mutate(lwr = fit - (1.96 * se.fit),
          upr = fit + (1.96 * se.fit),
          dev.60 = dev.60 * 100)
 
-zpred_raw %>% 
+zpred_wt %>% 
   ggplot() +
   geom_line(aes(y = fit, x = dev.60, color = as.character(num.bridge))) +
   geom_ribbon(aes(ymin = lwr, ymax = upr, x = dev.60, fill = as.character(num.bridge)), alpha = 0.3) +
@@ -171,8 +198,11 @@ zpred_raw %>%
        title = "Mean number of road crossings\nper year per 1300m road segment",
        color = "# bridges per\nroad segment",
        fill = "# bridges per\nroad segment") +
-  theme_bw()
+  theme_bw() + 
+  guides(colour = guide_legend(reverse=T),
+         fill = guide_legend(reverse=T))
 
+ggsave(here("figures/wt_mod_plot.png"), width = 5)
 
 # fit with weighted crossings as response
 wt_crossings_mods <- fit_summed_crossing_mods("tot.wt.cross")
