367619e86a6c8ca2198e145dcf8883e26ea59cf6
scottfjennings <scott.f.jennings@gmail.com>
2024-10-18 14:08:42 -0700

    make work and add plots for P1 and P5

--- code/output4_crossing_density_maps.R
+++ code/output4_crossing_density_maps.R
@@ -7,9 +7,15 @@ library(sf)
 library(ggmap)
 
 source("C:/Users/scott.jennings/OneDrive - Audubon Canyon Ranch/Projects/general_data_sources/google_api_key.R")
+source("C:/Users/scott.jennings/OneDrive - Audubon Canyon Ranch/Projects/general_data_sources/stadiamaps_api_key.R")
 
 summed_crossing_analysis_table <- readRDS(here("data/habitat_varbs_scales"))
 
+
+HR_UDS <- readRDS(here("model_objects/puma_hr_uds"))
+
+
+all_bbmm_ud <- readRDS(here("model_objects/all_bbmm_ud_1step"))
 
 napa_sonoma_rds_equal_segs <- readRDS(here("data/napa_sonoma_rds_equal_segs")) %>% 
   bind_rows() %>% 
@@ -20,11 +26,91 @@ equal_segs_crossings <- full_join(summed_crossing_analysis_table, napa_sonoma_rd
   st_as_sf() %>% 
   st_transform(crs = 4326)
 
+clean_bridges <- st_read("C:/Users/scott.jennings/OneDrive - Audubon Canyon Ranch/Projects/general_data_sources/roads/napa_sonoma_bridges/clean_bridges.shp") %>% 
+  st_transform(crs = 4326)
 
+# P1 2018
 p1_2018 <- filter(equal_segs_crossings, animal.id == "P1", year == 2018)
+p1_hr <- SpatialPolygonsDataFrame.UD(HR_UDS[["P1"]],level.UD=0.999,level=0.95) %>% 
+  st_as_sf() %>% 
+  filter(str_detect(name, "high")) %>% 
+  st_transform(crs = 4326)
+
+p1_bbob <- st_bbox(p1_hr)
 
 
-map <- get_googlemap("Santa Rosa, USA", zoom = 8, maptype = "terrain")
+p1_bridges <- st_intersection(p1_hr, clean_bridges)
 
-ggmap(map) +
-  geom_sf(data = p1_2018, aes(color = tot.wt.cross), linewidth = 3)
+p1_map <- get_stadiamap(c(left = -122.75, bottom = 38.25, right = -122.4, top = 38.5), maptype = "stamen_terrain")
+p1_map <- get_googlemap(center = c(lon = -122.533, lat = 38.365), zoom = 14, maptype = "satellite")
+
+ggmap(p1_map)+
+#ggplot()+
+  geom_sf(data = p1_hr, inherit.aes = FALSE, fill = NA, linewidth = 1) +
+  geom_sf(data = p1_2018, aes(color = tot.wt.cross), linewidth = 3, inherit.aes = FALSE) +
+  geom_sf(data = p1_bridges, inherit.aes = FALSE, color = "#08519C", size = 5) +
+  scale_color_gradient(low = "#FC9272", high = "#67000D") +
+  labs(x = "",
+       y = "",
+       color = "crossings/\nsegment",
+       title = "Mt Lion P1 in 2018")+
+  theme(axis.title=element_blank(),
+        axis.text=element_blank(),
+        axis.ticks=element_blank(),
+        legend.title=element_text(size=18),
+        legend.text=element_text(size=14),
+        plot.title = element_text(size=22))
+
+ggsave(here("figures/p1_2018_map.png"), dpi = 300)
+
+p1_map_zoom <- get_googlemap(center = c(lon = -122.533, lat = 38.365), zoom = 14, maptype = "satellite")
+
+ggmap(p1_map_zoom)+
+  #ggplot()+
+  geom_sf(data = p1_hr, inherit.aes = FALSE, fill = NA, linewidth = 1) +
+  geom_sf(data = p1_2018, aes(color = tot.wt.cross), linewidth = 3, inherit.aes = FALSE) +
+  geom_sf(data = p1_bridges, inherit.aes = FALSE, color = "#08519C", size = 5) +
+  scale_color_gradient(low = "#FC9272", high = "#67000D", guide="none") +
+  labs(x = "",
+       y = "",
+       color = "",
+       title = "")+
+  theme(axis.title=element_blank(),
+        axis.text=element_blank(),
+        axis.ticks=element_blank(),
+        plot.title = element_text(size=22))
+
+ggsave(here("figures/p1_2018_map_zoom.png"), dpi = 300)
+
+# P5 2018
+p5_2018 <- filter(equal_segs_crossings, animal.id == "P5", year == 2018)
+p5_hr <- SpatialPolygonsDataFrame.UD(HR_UDS[["P5"]],level.UD=0.999,level=0.95) %>% 
+  st_as_sf() %>% 
+  filter(str_detect(name, "high")) %>% 
+  st_transform(crs = 4326)
+
+p5_bbob <- st_bbox(p5_hr)
+
+
+p5_bridges <- st_intersection(p5_hr, clean_bridges)
+
+p5_map <- get_stadiamap(c(left = -122.8, bottom = 38.2, right = -122.3, top = 38.6), maptype = "stamen_terrain")
+
+ggmap(p5_map)+
+  #ggplot()+
+  geom_sf(data = p5_hr, inherit.aes = FALSE, fill = NA, linewidth = 1) +
+  geom_sf(data = p5_2018, aes(color = tot.wt.cross), linewidth = 1, inherit.aes = FALSE) +
+  geom_sf(data = p5_bridges, inherit.aes = FALSE, color = "#08519C", size = 3) +
+  scale_color_gradient(low = "#FC9272", high = "#67000D") +
+  labs(x = "",
+       y = "",
+       color = "crossings/\nsegment",
+       title = "Mt Lion P5 in 2018")+
+  theme(axis.title=element_blank(),
+        axis.text=element_blank(),
+        axis.ticks=element_blank(),
+        legend.title=element_text(size=18),
+        legend.text=element_text(size=14),
+        plot.title = element_text(size=22))
+
+ggsave(here("figures/p5_2018_map.png"), dpi = 300)
