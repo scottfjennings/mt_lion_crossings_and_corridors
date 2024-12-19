0f8a9ef7a1c957e14acaad9152c848dae6c2e337
scottfjennings <scott.f.jennings@gmail.com>
2024-12-19 14:39:47 -0800

    add several sub-summaries

--- code/output2_basic_data_summary.R
+++ code/output2_basic_data_summary.R
@@ -13,12 +13,58 @@ source(here("code/utilities.R"))
 source(here("code/helper_data.R"))
 
 # read data ----
+
+
+analysis_table <- readRDS(here("data/analysis_table"))  %>%
+  mutate(animal.id = ifelse(animal.id == "P5*", "P5", animal.id)) %>% 
+  filter(animal.id %in% analysis_pumas)
+
+
+puma_sexes <- read_xlsx("C:/Users/scott.jennings/OneDrive - Audubon Canyon Ranch/Projects/other_research/mt_lion_data_work/data/COLLAR DATE INFO_31Dec2023.xlsx")
+
+names(puma_sexes) <- tolower(names(puma_sexes))
+
+# number of males/females
+puma_sexes %>%
+  rename("animal.id" = `animal id`) %>% 
+  distinct(animal.id, sex) %>% 
+  filter(animal.id %in% analysis_pumas) %>% 
+  group_by(sex) %>% 
+  summarise(znum = n())
+
+
+
+# number of fixes per cat
+analysis_table %>% 
+  group_by(animal.id) %>% 
+  summarise(tot.gps = n()) %>% 
+  ungroup() %>% #view()
+  summarise(mean.gps = mean(tot.gps),
+            sd.gps = sd(tot.gps),
+            min.gps = min(tot.gps),
+            max.gps = max(tot.gps)) 
+
+# number of crossing steps per cat ----
+bbmm_crossed_equal_seg %>% 
+  bind_rows(., .id = "crossing.step") %>% 
+  data.frame() %>% 
+  select(-geometry) %>% 
+  mutate(animal.id = str_extract(crossing.step, "^[^_]+(?=_)")) %>%
+  distinct(animal.id, crossing.step) %>% 
+  group_by(animal.id) %>% 
+  summarise(tot.gps = n()) %>% 
+  ungroup() %>% 
+  summarise(mean.gps = mean(tot.gps),
+            sd.gps = sd(tot.gps),
+            min.gps = min(tot.gps),
+            max.gps = max(tot.gps)) 
+
 # steps
-puma_steps <- readRDS(here("data/puma_steps")) 
+puma_steps <- readRDS(here("data/puma_steps"))  %>% 
+  filter(!animal.id %in% exclude_pumas, !animal.id %in% hr_exclude_pumas)
 
 # number of lions 
-distinct(puma_steps, animal.id) %>% 
-  filter(!animal.id %in% exclude_pumas, !animal.id %in% hr_exclude_pumas) %>% nrow()
+distinct(puma_steps, animal.id) %>% nrow()
 
 # date range
 min(puma_steps$datetime.local)
