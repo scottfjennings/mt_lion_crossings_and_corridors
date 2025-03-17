7fd499d66e3e3603c6b1fa48a6f6191312dac45b
scottfjennings <scott.f.jennings@gmail.com>
2025-03-17 14:07:47 -0700

    adjust and rerun for class being assingned upstream. remove select from get_all_bbmm_equal_segs so class, seg.length get passed forward and don't need to be reattached or recalculated

--- code/B1_BBMM_segments.R
+++ code/B1_BBMM_segments.R
@@ -29,7 +29,8 @@ crossing_steps <- names(all_ud_rast)
 napa_sonoma_rds_equal_segs <- readRDS(here("data/napa_sonoma_rds_equal_segs"))
 # need this in a single dataframe for operations below
 napa_sonoma_rds_equal_segs_df <- napa_sonoma_rds_equal_segs %>% 
-  bind_rows() 
+  bind_rows() %>% 
+  full_join(readRDS(here("data/seg_midpoints_road_class")))
 
 
 # get all equal length road segments that are within the 90% UD for a step ----
@@ -55,7 +56,6 @@ get_all_bbmm_equal_segs <- function(zcrossing.step) {
   road_slice <- st_intersection(napa_sonoma_rds_equal_segs_df, road_slicer)
   
   bbmm_road_slice <- road_slice %>% 
-    select(seg.label, geometry) %>% 
     st_as_sf() %>% 
     st_intersection(st_as_sf(rp)) %>%
     # if a segment is split and there are multiple new segments in the crossing.step ud (e.g. "P13_37473_34190" and "Alta Monte Dr_Santa Rosa_1_1"), these end up as MULTILINESTRINGs and need to be broken back up into LINESTRINGs
@@ -63,7 +63,7 @@ get_all_bbmm_equal_segs <- function(zcrossing.step) {
     mutate(geometry = list(st_cast(geometry, "LINESTRING"))) %>% 
     unnest(geometry) %>% 
     st_as_sf() %>% 
-    mutate(crossed.seg.length = st_length(.)) %>% 
+    mutate(bbmm.seg.length = st_length(.)) %>% 
     group_by(seg.label) %>% 
     mutate(sub.seg.label = paste(seg.label, row_number(), sep = "_"))
   
@@ -88,57 +88,22 @@ bbmm_equal_seg <- map(bbmm_equal_seg_safe, "result")[sapply(map(bbmm_equal_seg_s
 bbmm_equal_seg_df <- bbmm_equal_seg %>% 
   bind_rows(., .id = "crossing.step") %>% 
   select(-X) %>% 
-  mutate(bbmm.seg.length = st_length(geometry))  %>% 
   mutate(animal.id = str_extract(crossing.step, "^[^_]+(?=_)"))
 
 
-equal_segs_lengths <- napa_sonoma_rds_equal_segs_df %>% 
-  rename("road.seg.length" = seg.length) %>% 
-  data.frame() %>% 
-  select(seg.label, road.seg.length, -geometry)
-
-crossing_years <- readRDS(here("data/crossing_clusters_gps_1step")) %>% 
-  mutate(year = year(datetime.local)) %>% 
-  distinct(crossing.step, year)
-
 # create a weighted crossing value based on the proportion of the equal length segment that was included in the BBMM UD
 bbmm_equal_seg_weights <- bbmm_equal_seg_df %>% 
-  left_join(equal_segs_lengths) %>% 
   mutate(raw.crossing = 1,
-         bbmm.segment.weight = as.numeric(bbmm.seg.length/road.seg.length)) %>% 
-#  left_join(crossing_years) %>% 
+         bbmm.segment.weight = as.numeric(bbmm.seg.length/seg.length)) %>% 
   filter(!animal.id %in% hr_exclude_pumas,
          !seg.label %in% p31_exclude_segments,
          !st_is(., c("POINT", "MULTIPOINT"))) %>%  # end up with some point objects, want only lines 
-  distinct() # and still somehow ending up with duplicates - still don't know where/how this is happening
+  select(crossing.step, seg.length, seg.label, class, geometry, bbmm.seg.length, sub.seg.label, animal.id, raw.crossing, bbmm.segment.weight)
 
   
 saveRDS(bbmm_equal_seg_weights, here("data/bbmm_equal_seg_weights"))
 
 
-
-
-
-# zsegment = "Buhman Ave_1"
-
-
-crossings_per_segment_crossed <- wt_road_crossed_segs %>% 
-  data.frame() %>% 
-  select(-geometry) %>% 
-  group_by(seg.label) %>% 
-  summarise(raw.crossings.per.segment = sum(raw.crossing),
-            weighted.crossings.per.segment = sum(weighted.crossing)) %>% 
-  ungroup() 
-
-crossings_per_segment_all <- crossings_per_segment_crossed %>% 
-  full_join(napa_sonoma_rds_equal_segs_df) %>% 
-  mutate(across(contains("crossings.per.segment"), ~replace_na(., 0))) %>% 
-  st_as_sf()
-
-
-ggplot() +
-  geom_sf(data = filter(crossings_per_segment_all, weighted.crossings.per.segment == 0), color = "gray") +
-  geom_sf(data = filter(crossings_per_segment_all, weighted.crossings.per.segment > 0), aes(color = weighted.crossings.per.segment))
 
 
   
