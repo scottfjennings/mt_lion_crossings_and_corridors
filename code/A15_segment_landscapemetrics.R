11ddc49e78065a3d614de1df5155b0abae236855
scottfjennings <scott.f.jennings@gmail.com>
2025-05-20 13:17:45 -0700

    updates for new reduced combined homerange

--- code/A15_segment_landscapemetrics.R
+++ code/A15_segment_landscapemetrics.R
@@ -12,43 +12,35 @@ library(here)
 library(tidyverse)
 
 
-# home range utilization distributions from A2_fit_ctmm_homerange.R
-hr_uds <- readRDS(here("model_objects/puma_hr_uds"))
+# roads 
+# this is just the segments in the combined homerange polygon for the 12 analysis lions
+segments_in_combined_homeranges <- readRDS(here("data/segments_in_combined_homeranges"))
 
 
-# roads
-napa_sonoma_rds_equal_segs <- readRDS(here("data/napa_sonoma_rds_equal_segs")) %>% 
-  bind_rows() %>% 
-  rename("road.seg.length" = seg.length)
 
-combined_hr <- st_read(here("data/shapefiles/combined_puma_homeranges_99.shp")) %>%
-  st_transform(26910)
-
-combined_hr_masker <- sf::st_buffer(combined_hr, 300)
-
-
-hr_segs <- napa_sonoma_rds_equal_segs[sf::st_intersects(napa_sonoma_rds_equal_segs, combined_hr, sparse = FALSE), ]  %>%
-  st_transform(26910)
-
-puma_years <- readRDS(here("data/crossing_clusters_gps_1step")) %>% 
-  mutate(year = year(datetime.local), 
-         puma = str_extract(crossing.step, "^[^_]+(?=_)"),
-         puma = ifelse(puma == "P5*", "P5", puma)) %>% 
+study_years <- readRDS(here("data/crossing_clusters_gps_1step")) %>% 
+  mutate(year = year(datetime.local)#, 
+         #puma = str_extract(crossing.step, "^[^_]+(?=_)"),
+         #puma = ifelse(puma == "P5*", "P5", puma)
+  ) %>% 
   ungroup() %>% 
-  distinct(puma, year)
+  distinct(year) %>% 
+  pull(year) # convert df column to string
+
+# redoing this to NOT be by puma
+# May 2025 realized doing this by puma means extracting habitat multiple time for each segment where home ranges overlap.
+# quicker to just do each segment once
+
+# reading in data takes some time so doing it once outside the function
+
+combined_puma_homeranges_95 <- st_read(here("data/shapefiles/combined_puma_homeranges_95.shp"))
+# this is to filter the habitat raster to speed processing time when extracting values around each road segment. need the 300 m buffer to get all the habitat for segments that are right on the homerange edge.
+hab_masker <- st_buffer(combined_puma_homeranges_95, 300)
 
 # TRE layer only ----
-get_hr_road_landmetrics_tre <- function(zpuma, zyear) {
+get_hr_road_landmetrics_tre <- function(zyear) {
   thresholds <- c(25, 50, 75)
   buffers <- c(100, 200, 300)
-
-  # Get home range polygon
-  puma_hr_uds <- hr_uds[[zpuma]] %>%
-    ctmm::as.sf(DF = "PDF", level.UD = 0.999) %>%
-    st_transform(26910) %>%
-    dplyr::filter(stringr::str_detect(name, "est"))
-
-  hab_masker <- sf::st_buffer(puma_hr_uds, 300)
 
   # Load and process habitat raster
   zhab <- terra::rast(paste0(
@@ -64,11 +56,6 @@ get_hr_road_landmetrics_tre <- function(zpuma, zyear) {
   binary_forest_list <- lapply(thresholds, function(thresh) terra::classify(hr_hab > thresh, cbind(TRUE, 1), others = 0))
   names(binary_forest_list) <- as.character(thresholds)
 
-  # Subset roads within home range using spatial filter
-  hr_roads <- napa_sonoma_rds_equal_segs[sf::st_intersects(napa_sonoma_rds_equal_segs, puma_hr_uds, sparse = FALSE), ] %>%
-    sf::st_transform(sf::st_crs(hr_hab)) %>%
-    dplyr::mutate(hr.seg.length = sf::st_length(.)) %>%
-    dplyr::select(seg.label, geometry)
 
   combos <- expand.grid(buff = buffers, forest.threshold = thresholds)
 
@@ -78,11 +65,11 @@ get_hr_road_landmetrics_tre <- function(zpuma, zyear) {
 
     forest_binary <- binary_forest_list[[as.character(threshold)]]
 
-    road_vect <- terra::vect(hr_roads)
+    road_vect <- terra::vect(segments_in_combined_homeranges)
     road_buffer <- terra::buffer(road_vect, width = buff_dist)
     road_buffer_sf <- sf::st_as_sf(road_buffer)
 
-    road_buffer_sf$seg.label <- hr_roads$seg.label
+    road_buffer_sf$seg.label <- segments_in_combined_homeranges$seg.label
 
     buffer_metrics <- purrr::map_dfr(1:nrow(road_buffer_sf), function(j) {
       one_buf <- road_buffer_sf[j, ]
@@ -114,7 +101,7 @@ get_hr_road_landmetrics_tre <- function(zpuma, zyear) {
           forest.threshold = threshold)
     })
 
-    dplyr::left_join(hr_roads, buffer_metrics, by = c("seg.label")) %>%
+    dplyr::left_join(segments_in_combined_homeranges, buffer_metrics, by = c("seg.label")) %>%
       dplyr::filter(!is.na(buff))
   })
 
@@ -138,17 +125,10 @@ zz %>%
 
 
 # TRE and TRE + SHR ----
-get_hr_road_landmetrics_treshr <- function(zpuma, zyear) {
+get_hr_road_landmetrics_treshr <- function(zyear) {
   thresholds <- c(25, 50, 75)
   buffers <- c(100, 200, 300)
   
-  # Get home range polygon
-  puma_hr_uds <- hr_uds[[zpuma]] %>%
-    ctmm::as.sf(DF = "PDF", level.UD = 0.999) %>%
-    st_transform(26910) %>%
-    dplyr::filter(stringr::str_detect(name, "est"))
-  
-  hab_masker <- sf::st_buffer(puma_hr_uds, 300)
   
   # Load and process habitat raster
   zhab <- terra::rast(paste0(
@@ -169,11 +149,6 @@ get_hr_road_landmetrics_treshr <- function(zpuma, zyear) {
     binary_forest_list <- lapply(thresholds, function(thresh) terra::classify(hr_hab > thresh, cbind(TRUE, 1), others = 0))
     names(binary_forest_list) <- as.character(thresholds)
     
-    hr_roads <- napa_sonoma_rds_equal_segs[sf::st_intersects(napa_sonoma_rds_equal_segs, puma_hr_uds, sparse = FALSE), ] %>%
-      sf::st_transform(sf::st_crs(hr_hab)) %>%
-      dplyr::mutate(hr.seg.length = sf::st_length(.)) %>%
-      dplyr::select(seg.label, geometry)
-    
     combos <- expand.grid(buff = buffers, forest.threshold = thresholds)
     
     results <- purrr::map_dfr(1:nrow(combos), function(i) {
@@ -182,10 +157,10 @@ get_hr_road_landmetrics_treshr <- function(zpuma, zyear) {
       
       forest_binary <- binary_forest_list[[as.character(threshold)]]
       
-      road_vect <- terra::vect(hr_roads)
+      road_vect <- terra::vect(segments_in_combined_homeranges)
       road_buffer <- terra::buffer(road_vect, width = buff_dist)
       road_buffer_sf <- sf::st_as_sf(road_buffer)
-      road_buffer_sf$seg.label <- hr_roads$seg.label
+      road_buffer_sf$seg.label <- segments_in_combined_homeranges$seg.label
       
       buffer_metrics <- purrr::map_dfr(1:nrow(road_buffer_sf), function(j) {
         one_buf <- road_buffer_sf[j, ]
@@ -294,9 +269,9 @@ get_hr_road_patch_cohesion_treshr <- function(zyear) {
   sum_layer <- tre_layer + shr_layer
   
   process_layer <- function(layer, name_suffix) {
-    rast_cropped <- terra::crop(layer, combined_hr_masker)
+    rast_cropped <- terra::crop(layer, hab_masker)
     rast_proj <- terra::project(rast_cropped, "EPSG:26910")
-    hr_hab <- terra::mask(rast_proj, combined_hr_masker)
+    hr_hab <- terra::mask(rast_proj, hab_masker)
     
     binary_forest_list <- lapply(thresholds, function(thresh) terra::classify(hr_hab > thresh, cbind(TRUE, 1), others = 0))
     names(binary_forest_list) <- as.character(thresholds)
@@ -310,10 +285,10 @@ get_hr_road_patch_cohesion_treshr <- function(zyear) {
       
       forest_binary <- binary_forest_list[[as.character(threshold)]]
       
-      road_vect <- terra::vect(hr_segs)
+      road_vect <- terra::vect(segments_in_combined_homeranges)
       road_buffer <- terra::buffer(road_vect, width = buff_dist)
       road_buffer_sf <- sf::st_as_sf(road_buffer)
-      road_buffer_sf$seg.label <- hr_segs$seg.label
+      road_buffer_sf$seg.label <- segments_in_combined_homeranges$seg.label
       
       buffer_metrics <- purrr::map_dfr(1:nrow(road_buffer_sf), function(j) {
         one_buf <- road_buffer_sf[j, ]
@@ -337,7 +312,7 @@ get_hr_road_patch_cohesion_treshr <- function(zyear) {
           )
       })
       
-      dplyr::left_join(hr_segs, buffer_metrics, by = c("seg.label")) %>%
+      dplyr::left_join(segments_in_combined_homeranges, buffer_metrics, by = c("seg.label")) %>%
         dplyr::filter(!is.na(buff))
     })
     
